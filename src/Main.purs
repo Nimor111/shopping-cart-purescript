9ae7ab66ec007e1fbca61d06f2497e97b5631c4c
Nimor111 <georgi.bojinov@hotmail.com>
2020-04-11 12:49:36 +0300

    Move running server to a separate module

--- src/Main.purs
+++ src/Main.purs
@@ -2,24 +2,9 @@ module Main where
 
 import Prelude
 
-import Data.Either (Either(..))
-import Database.PostgreSQL as PostgreSQL
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class (liftEffect)
-import Effect.Class.Console (log, logShow)
-import Web.ShoppingCart.Database (dbConfig, createPeople)
-import Web.ShoppingCart.Server (server)
+import Web.ShoppingCart (runServer) as ShoppingCart
 
 
 main :: Effect Unit
-main = launchAff_ do
-    pool <- liftEffect $ PostgreSQL.newPool dbConfig
-    PostgreSQL.withConnection pool case _ of
-      Left pgError -> logShow ("PostgreSQL Connection Error: " <> show pgError)
-      Right conn -> do
-         log "Starting server..."
-         createPeople conn
-         log "Created people..."
-
-         void $ liftEffect $ server { conn, other: "other", jwtSecret: "tOpSeCrEt" }
+main = ShoppingCart.runServer
---
+++ src/ShoppingCart.purs
@@ -0,0 +1,25 @@
+module Web.ShoppingCart where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Database.PostgreSQL as PostgreSQL
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (log, logShow)
+import Web.ShoppingCart.Database (dbConfig, createPeople)
+import Web.ShoppingCart.Server (server)
+
+
+runServer :: Effect Unit
+runServer = launchAff_ do
+    pool <- liftEffect $ PostgreSQL.newPool dbConfig
+    PostgreSQL.withConnection pool case _ of
+      Left pgError -> logShow ("PostgreSQL Connection Error: " <> show pgError)
+      Right conn -> do
+         log "Starting server..."
+         createPeople conn
+         log "Created people..."
+
+         void $ liftEffect $ server { conn, other: "other", jwtSecret: "tOpSeCrEt" }
