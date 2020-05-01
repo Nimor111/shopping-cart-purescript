ca2bfc53a93c4cb80f9123629490bd7f90d90363
Nimor111 <georgi.bojinov@hotmail.com>
2020-05-01 20:59:26 +0300

    Remove redundant function

--- src/Web/ShoppingCart/Router.purs
+++ src/Web/ShoppingCart/Router.purs
@@ -66,25 +66,6 @@ router routes request@{ path } = do
   subpath :: HTTPure.Path -> HTTPure.Path
   subpath prefix = Array.drop (Array.length prefix) path
 
-{--insertPeople ::--}
-{--∀ r m.--}
-{--MonadAff m =>--}
-{--MonadAsk Context m =>--}
-{--MonadThrow (AppError r) m =>--}
-{--HTTPure.Request ->--}
-{--m HTTPure.Response--}
-{--insertPeople _ =--}
-{--hoistSelda (insert_ people peopleData)--}
-{-->>= (\_ -> HTTPure.ok $ "Inserted.")--}
-{--where--}
-{--peopleData :: Array { id :: Int, name :: String, age :: Maybe Int }--}
-{--peopleData =--}
-{--[ { id: 1, name: "name1", age: Just 11 }--}
-{--, { id: 2, name: "name2", age: Just 22 }--}
-{--, { id: 3, name: "name3", age: Just 33 }--}
-{--, { id: 4, name: "name4", age: Just 44 }--}
-{--, { id: 5, name: "name5", age: Just 55 }--}
-{--]--}
 errorOut ::
   ∀ r m.
   MonadAff m =>
